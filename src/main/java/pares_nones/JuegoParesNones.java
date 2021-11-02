213365205a291cd57efb30b3662310dd95d667ec
Hind <hindsamiri2@gmail.com>
2021-11-02 07:44:26 -0400

    añadir codigo para saber que jugador gana

--- src/main/java/pares_nones/JuegoParesNones.java
+++ src/main/java/pares_nones/JuegoParesNones.java
@@ -11,7 +11,7 @@ public class JuegoParesNones {
     public static void main(String[] args) {
         
         //declaracion y initializacion variables
-        int opcion,opcionP,pare,none;
+        int opcion,opcionP,pare = 0,noneP = 0,suma;
         boolean esPar;
         Scanner entrada=new Scanner(System.in);
         
@@ -36,20 +36,32 @@ public class JuegoParesNones {
                      System.out.println("2.Nones");
                      
                      switch(opcionP){
+                         //jugador A coge una opcion
                          case 1:
                              System.out.println("Elija un numero pare");
                              pare=entrada.nextInt();
-                             if(pare%2==0){
-                                 esPar=true;
-                             }else{
-                                 esPar=false;
-                                 System.out.println("El numero no es Par eliga otro");
-                                 pare=entrada.nextInt();
-                             }
+                             esPar=true;
                              break;
                              
-                         
+                         case 2:
+                             System.out.println("Elija un numero nonePare");
+                             noneP=entrada.nextInt();
+                             esPar=false;
+                          break;
+         
                      }
+                         //el jugador B lo queda una opcion
+                            System.out.println("que numero quieres eligir: ");
+                            if(esPar=true){
+                             System.out.println("Elija un numero nonePare");
+                             noneP=entrada.nextInt();
+                            }
+                            suma=noneP+pare;
+                            if(suma%2==0){
+                                System.out.println("el jugador que gana es A porque el resulta es par");
+                            }else{
+                                System.out.println("el jugador que gana es B porque el resulta es Nonepar");
+                            }
                      
                 
                 
